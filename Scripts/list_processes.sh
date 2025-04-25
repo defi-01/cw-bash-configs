17bebd2582602df7a3f29fb163ad4b9012160ad7
Dionysus <125485464+defi-01@users.noreply.github.com>
2025-04-26 00:32:30 +0300

    Update list_processes.sh

--- Scripts/list_processes.sh
+++ Scripts/list_processes.sh
@@ -1,4 +1,5 @@
 # bash script list_processes.sh that uses the ps command to list all currently running processes, including their respective command names, process IDs (PIDs), and parent process IDs (PPIDs).
+
 #!/bin/bash
 
 ps -eo cmd,pid,ppid
