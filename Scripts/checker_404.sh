66d77729d774ec41a0633e9325fa4274dde7d6c6
Dionysus <125485464+defi-01@users.noreply.github.com>
2025-04-26 00:31:00 +0300

    Update checker_404.sh

--- Scripts/checker_404.sh
+++ Scripts/checker_404.sh
@@ -1,4 +1,4 @@
-# a simple script that reads some log file apache_logs and writes into the file not_found.txt information about address, HTTP method and path from all requests where a 404 status code was thrown.
+# A simple script that reads some log file, apache_logs, and writes into the file not_found.txt information about the address, HTTP method, and path from all requests where a 404 status code was thrown.
 #!/bin/bash
 
 cat apache_logs | grep ' 404 ' | awk '{print $1, $6, $7}' > not_found.txt
